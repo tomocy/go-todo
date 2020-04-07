b9d48d50b49f4f0f36ad9c808fd9d18b332a0975
tomocy <tomocy.dev@gmail.com>
2020-04-07 19:22:17 +0900

    update createTask
    createTask can create task.

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"context"
+	"fmt"
 	"time"
 
 	"github.com/tomocy/go-todo"
@@ -11,5 +13,20 @@ type createTask struct {
 }
 
 func (u *createTask) createTask(name string, dueDate time.Time) error {
+	ctx := context.Background()
 
+	id, err := u.repo.NextID(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to generate task id: %w", err)
+	}
+	t, err := todo.NewTask(id, name, dueDate)
+	if err != nil {
+		return fmt.Errorf("failed to generate task: %w", err)
+	}
+
+	if err := u.repo.Save(ctx, t); err != nil {
+		return fmt.Errorf("failed to save task: %w", err)
+	}
+
+	return nil
 }
