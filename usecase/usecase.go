f97ad41133c39423e4f5c55fa04b7cc6725d5393
tomocy <tomocy.dev@gmail.com>
2020-04-07 19:34:21 +0900

    update createUser
    createUser can create user.

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -12,6 +12,25 @@ type createUser struct {
 	repo todo.UserRepo
 }
 
+func (u *createUser) createUser(name, email, password string) error {
+	ctx := context.Background()
+
+	id, err := u.repo.NextID(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to generate user id: %w", err)
+	}
+	user, err := todo.NewUser(id, name, email, password)
+	if err != nil {
+		return fmt.Errorf("failed to generate user: %w", err)
+	}
+
+	if err := u.repo.Save(ctx, user); err != nil {
+		return fmt.Errorf("failed to save user: %w", err)
+	}
+
+	return nil
+}
+
 type createTask struct {
 	repo todo.TaskRepo
 }
