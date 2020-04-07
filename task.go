d74845caef4cfb616b777b8fa25e5aa88b03198e
tomocy <tomocy.dev@gmail.com>
2020-04-07 19:35:28 +0900

    refactor files

--- task.go
+++ task.go
@@ -6,65 +6,10 @@ import (
 	"time"
 )
 
-type UserRepo interface {
-	NextID(context.Context) (userID, error)
-	Save(context.Context, *user) error
-}
-
-func NewUser(id userID, name, email, password string) (*user, error) {
-	if name == "" {
-		name = string(id)
-	}
-	if email == "" {
-		return nil, fmt.Errorf("empty email")
-	}
-	if password == "" {
-		return nil, fmt.Errorf("empty password")
-	}
-
-	return &user{
-		id:   id,
-		name: name,
-		profile: profile{
-			email: email,
-		},
-		cred: cred{
-			password: password,
-		},
-	}, nil
-}
-
-type user struct {
-	id      userID
-	name    string
-	status  userStatus
-	profile profile
-	cred    cred
-}
-
-type userID string
-
-type userStatus int
-
-const (
-	userActive userStatus = iota
-	userInactive
-)
-
-type profile struct {
-	email string
-}
-
-type cred struct {
-	password string
-}
-
 type TaskRepo interface {
 	NextID(context.Context) (taskID, error)
 	Save(context.Context, *task) error
 }
-
-const postponedMaxTimes = 3
 
 func NewTask(id taskID, name string, dueDate time.Time) (*task, error) {
 	if id == "" {
@@ -89,6 +34,8 @@ type task struct {
 	dueDate        time.Time
 	postponedTimes int
 }
+
+const postponedMaxTimes = 3
 
 func (t *task) postpone() error {
 	if t.postponedTimes > postponedMaxTimes {
---
+++ user.go
@@ -0,0 +1,59 @@
+package todo
+
+import (
+	"context"
+	"fmt"
+)
+
+type UserRepo interface {
+	NextID(context.Context) (userID, error)
+	Save(context.Context, *user) error
+}
+
+func NewUser(id userID, name, email, password string) (*user, error) {
+	if name == "" {
+		name = string(id)
+	}
+	if email == "" {
+		return nil, fmt.Errorf("empty email")
+	}
+	if password == "" {
+		return nil, fmt.Errorf("empty password")
+	}
+
+	return &user{
+		id:   id,
+		name: name,
+		profile: profile{
+			email: email,
+		},
+		cred: cred{
+			password: password,
+		},
+	}, nil
+}
+
+type user struct {
+	id      userID
+	name    string
+	status  userStatus
+	profile profile
+	cred    cred
+}
+
+type userID string
+
+type userStatus int
+
+const (
+	userActive userStatus = iota
+	userInactive
+)
+
+type profile struct {
+	email string
+}
+
+type cred struct {
+	password string
+}
