525ffa89437bb5889ad10f36975553ea3f2c74aa
tomocy <tomocy.dev@gmail.com>
2020-04-11 19:11:36 +0900

    update TaskRepo
    TaskRepo can generate task id.

--- infra/memory/task.go
+++ infra/memory/task.go
@@ -1,11 +1,17 @@
 package memory
 
 import (
+	"context"
+
 	"github.com/tomocy/go-todo"
 )
 
 type TaskRepo struct {
 	tasks map[todo.TaskID]*todo.Task
+}
+
+func (r *TaskRepo) NextID(context.Context) (todo.TaskID, error) {
+	return todo.TaskID(generateRandomString(50)), nil
 }
 
 func (r *TaskRepo) initIfNecessary() {
