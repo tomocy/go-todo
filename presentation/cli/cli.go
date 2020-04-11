ee52363c43beb69afff9ae28ecc670a5d8df5ff8
tomocy <tomocy.dev@gmail.com>
2020-04-11 20:57:14 +0900

    update app
    App can provide task repo.

--- presentation/cli/cli.go
+++ presentation/cli/cli.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"github.com/tomocy/go-todo"
+	"github.com/tomocy/go-todo/infra/memory"
 	"github.com/urfave/cli"
 )
 
@@ -12,4 +14,8 @@ func (a *app) init() {
 	a.App = cli.NewApp()
 	a.Name = "todo"
 	a.Commands = []cli.Command{}
+}
+
+func (a *app) taskRepo() todo.TaskRepo {
+	return new(memory.TaskRepo)
 }
