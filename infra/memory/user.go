dd36a1cd88ffa1442fc96bafa078dbaf0a0e3b54
tomocy <tomocy.dev@gmail.com>
2020-04-11 18:33:17 +0900

    update UserRepo
    UserRepo can find user by email.

--- infra/memory/user.go
+++ infra/memory/user.go
@@ -2,6 +2,7 @@ package memory
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/tomocy/go-todo"
 )
@@ -12,4 +13,14 @@ type UserRepo struct {
 
 func (r *UserRepo) NextID(context.Context) (todo.UserID, error) {
 	return todo.UserID(generateRandomString(30)), nil
+}
+
+func (r *UserRepo) FindByEmail(_ context.Context, email string) (*todo.User, error) {
+	for _, u := range r.users {
+		if u.Email() == email {
+			return u, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no such user")
 }
