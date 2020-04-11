a404e6da3cbb9ac4f861c26087404ee748a5832c
tomocy <tomocy.dev@gmail.com>
2020-04-11 21:12:05 +0900

    update todo command
    Todo command runs a cli app.

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/tomocy/go-todo/presentation/cli"
+)
+
+func main() {
+	app := cli.NewApp(os.Stdout)
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
