2447c854c12a290bb1a0f7ad1958c04a3693dee1
tomocy <tomocy.dev@gmail.com>
2020-04-11 18:28:37 +0900

    update UserRepo
    UserRepo can generate next user id.

--- infra/memory/user.go
+++ infra/memory/user.go
@@ -1,9 +1,15 @@
 package memory
 
 import (
+	"context"
+
 	"github.com/tomocy/go-todo"
 )
 
 type UserRepo struct {
 	users []*todo.User
+}
+
+func (r *UserRepo) NextID(context.Context) (todo.UserID, error) {
+	return todo.UserID(generateRandomString(30)), nil
 }
