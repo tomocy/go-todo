3434219ca1c7ac0308e47881390460737c742f2d
tomocy <tomocy.dev@gmail.com>
2020-04-11 19:13:06 +0900

    update TaskRepo
    TaskRepo can save task.

--- infra/memory/task.go
+++ infra/memory/task.go
@@ -14,6 +14,14 @@ func (r *TaskRepo) NextID(context.Context) (todo.TaskID, error) {
 	return todo.TaskID(generateRandomString(50)), nil
 }
 
+func (r *TaskRepo) Save(_ context.Context, t *todo.Task) error {
+	r.initIfNecessary()
+
+	r.tasks[t.ID()] = t
+
+	return nil
+}
+
 func (r *TaskRepo) initIfNecessary() {
 	if r.tasks == nil {
 		r.tasks = make(map[todo.TaskID]*todo.Task)
