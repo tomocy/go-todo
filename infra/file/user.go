45248f21609c2217b7d6af24847e71dd99ec50e8
tomocy <tomocy.dev@gmail.com>
2020-04-11 22:35:28 +0900

    add user

--- infra/file/user.go
+++ infra/file/user.go
@@ -14,3 +14,11 @@ type userRepo struct {
 func (r *userRepo) NextID(context.Context) (todo.UserID, error) {
 	return todo.UserID(generateRandomString(30)), nil
 }
+
+type user struct {
+	ID       todo.UserID     `json:"id"`
+	Name     string          `json:"name"`
+	Email    string          `json:"email"`
+	Password todo.Password   `json:"password"`
+	Status   todo.UserStatus `json:"status"`
+}
