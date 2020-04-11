3a9736f7a7ffb5ee399b9f450dc52a357fd806f9
tomocy <tomocy.dev@gmail.com>
2020-04-11 18:07:35 +0900

    update createUser
    createUser returns the created User.

--- usecase/user.go
+++ usecase/user.go
@@ -11,27 +11,27 @@ type createUser struct {
 	repo todo.UserRepo
 }
 
-func (u *createUser) do(name, email, password string) error {
+func (u *createUser) do(name, email, password string) (*todo.User, error) {
 	ctx := context.TODO()
 
 	id, err := u.repo.NextID(ctx)
 	if err != nil {
-		return fmt.Errorf("failed to generate user id: %w", err)
+		return nil, fmt.Errorf("failed to generate user id: %w", err)
 	}
 	hashed, err := todo.HashPassword(password)
 	if err != nil {
-		return fmt.Errorf("failed to hash password: %w", err)
+		return nil, fmt.Errorf("failed to hash password: %w", err)
 	}
 	user, err := todo.NewUser(id, name, email, hashed)
 	if err != nil {
-		return fmt.Errorf("failed to generate user: %w", err)
+		return nil, fmt.Errorf("failed to generate user: %w", err)
 	}
 
 	if err := u.repo.Save(ctx, user); err != nil {
-		return fmt.Errorf("failed to save user: %w", err)
+		return nil, fmt.Errorf("failed to save user: %w", err)
 	}
 
-	return nil
+	return user, nil
 }
 
 type authenticateUser struct {
