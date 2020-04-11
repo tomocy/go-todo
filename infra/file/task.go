4a7f785280e93e36f52cca63626b0096cef433b9
tomocy <tomocy.dev@gmail.com>
2020-04-11 21:31:46 +0900

    add task

--- infra/file/task.go
+++ infra/file/task.go
@@ -1,8 +1,21 @@
 package file
 
-import "github.com/tomocy/go-todo"
+import (
+	"time"
+
+	"github.com/tomocy/go-todo"
+)
 
 type taskRepo struct {
 	fname string
 	tasks map[todo.TaskID]*todo.Task
+}
+
+type task struct {
+	ID             todo.TaskID     `json:"id"`
+	UserID         todo.UserID     `json:"user_id"`
+	Name           string          `json:"name"`
+	Status         todo.TaskStatus `json:"status"`
+	DueDate        time.Time       `json:"due_date"`
+	PostponedTimes int             `json:"postponed_times"`
 }
