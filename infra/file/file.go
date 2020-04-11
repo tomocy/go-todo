cdf30b66ebe36c8f88eb45a0a43513a160b16431
tomocy <tomocy.dev@gmail.com>
2020-04-11 21:24:43 +0900

    add load

--- infra/file/file.go
+++ infra/file/file.go
@@ -6,6 +6,19 @@ import (
 	"os"
 )
 
+func load(fname string, dst interface{}) error {
+	src, err := os.Open(fname)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+
+	if err := json.NewDecoder(src).Decode(dst); err != nil {
+		return fmt.Errorf("failed to decode: %w", err)
+	}
+
+	return nil
+}
+
 func save(fname string, v interface{}) error {
 	dst, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
