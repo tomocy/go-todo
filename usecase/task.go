070fa2da4eb4aabc2938cd04f2e888061d28608a
tomocy <tomocy.dev@gmail.com>
2020-04-11 16:59:32 +0900

    refactor files

---
+++ usecase/task.go
@@ -0,0 +1,32 @@
+package usecase
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/tomocy/go-todo"
+)
+
+type createTask struct {
+	repo todo.TaskRepo
+}
+
+func (u *createTask) createTask(name string, dueDate time.Time) error {
+	ctx := context.Background()
+
+	id, err := u.repo.NextID(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to generate task id: %w", err)
+	}
+	t, err := todo.NewTask(id, name, dueDate)
+	if err != nil {
+		return fmt.Errorf("failed to generate task: %w", err)
+	}
+
+	if err := u.repo.Save(ctx, t); err != nil {
+		return fmt.Errorf("failed to save task: %w", err)
+	}
+
+	return nil
+}
--- usecase/user.go
+++ usecase/user.go
@@ -3,7 +3,6 @@ package usecase
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/tomocy/go-todo"
 )
@@ -26,29 +25,6 @@ func (u *createUser) createUser(name, email, password string) error {
 
 	if err := u.repo.Save(ctx, user); err != nil {
 		return fmt.Errorf("failed to save user: %w", err)
-	}
-
-	return nil
-}
-
-type createTask struct {
-	repo todo.TaskRepo
-}
-
-func (u *createTask) createTask(name string, dueDate time.Time) error {
-	ctx := context.Background()
-
-	id, err := u.repo.NextID(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to generate task id: %w", err)
-	}
-	t, err := todo.NewTask(id, name, dueDate)
-	if err != nil {
-		return fmt.Errorf("failed to generate task: %w", err)
-	}
-
-	if err := u.repo.Save(ctx, t); err != nil {
-		return fmt.Errorf("failed to save task: %w", err)
 	}
 
 	return nil
