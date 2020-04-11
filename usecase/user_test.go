cb74119a522b8baf7eebee1e9d2c72b855731775
tomocy <tomocy.dev@gmail.com>
2020-04-11 19:05:35 +0900

    add TestAuthenticateUser

--- usecase/user_test.go
+++ usecase/user_test.go
@@ -28,6 +28,31 @@ func TestCreateUser(t *testing.T) {
 	}
 }
 
+func TestAuthenticateUser(t *testing.T) {
+	repo := new(memory.UserRepo)
+	createUsecase := createUser{
+		repo: repo,
+	}
+
+	name, email, pass := "name", "email", "pass"
+	createUsecase.do(name, email, pass)
+
+	u := authenticateUser{
+		repo: repo,
+	}
+
+	user, err := u.do(email, pass)
+	if err != nil {
+		t.Errorf("should have authenticated user: %s", err)
+		return
+	}
+
+	if err := assertUser(user, name, email, pass); err != nil {
+		t.Errorf("should have returned the create user: %s", err)
+		return
+	}
+}
+
 func assertUser(u *todo.User, name, email, pass string) error {
 	if u.Name() != name {
 		return reportUnexpected("name", u.Name(), name)
