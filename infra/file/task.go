f6293a8a190862d2a2bcc9b7157b36e90a81dbf9
tomocy <tomocy.dev@gmail.com>
2020-04-11 21:41:30 +0900

    update taskRepo
    TaskRepo can load tasks.

--- infra/file/task.go
+++ infra/file/task.go
@@ -11,6 +11,20 @@ type taskRepo struct {
 	tasks map[todo.TaskID]*todo.Task
 }
 
+func (r *taskRepo) load() error {
+	var ts []*task
+	if err := load(r.fname, ts); err != nil {
+		return err
+	}
+
+	for _, t := range ts {
+		adapted := t.adapt()
+		r.tasks[adapted.ID()] = adapted
+	}
+
+	return nil
+}
+
 type task struct {
 	ID             todo.TaskID     `json:"id"`
 	UserID         todo.UserID     `json:"user_id"`
