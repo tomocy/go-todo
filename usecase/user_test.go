904d62b5434a5561f55488b2a7ccb347052ba217
tomocy <tomocy.dev@gmail.com>
2020-04-11 19:02:03 +0900

    add TestCreateUser

---
+++ usecase/user_test.go
@@ -0,0 +1,43 @@
+package usecase
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/tomocy/go-todo"
+	"github.com/tomocy/go-todo/infra/memory"
+)
+
+func TestCreateUser(t *testing.T) {
+	repo := new(memory.UserRepo)
+	u := createUser{
+		repo: repo,
+	}
+
+	name, email, pass := "name", "email", "pass"
+
+	user, err := u.do(name, email, pass)
+	if err != nil {
+		t.Errorf("should have created user: %s", err)
+		return
+	}
+
+	if err := assertUser(user, name, email, pass); err != nil {
+		t.Errorf("should have returned the create user: %s", err)
+		return
+	}
+}
+
+func assertUser(u *todo.User, name, email, pass string) error {
+	if u.Name() != name {
+		return reportUnexpected("name", u.Name(), name)
+	}
+	if u.Email() != email {
+		return reportUnexpected("email", u.Email(), email)
+	}
+	if !u.Password().IsSame(pass) {
+		return fmt.Errorf("unexpected password: password is not correct")
+	}
+
+	return nil
+}
