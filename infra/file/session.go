4e564a060fa394d7fa4c668bfdc95cd25373e009
tomocy <tomocy.dev@gmail.com>
2020-04-12 15:10:30 +0900

    update sessionRepo
    SessionRepo can pull session.

--- infra/file/session.go
+++ infra/file/session.go
@@ -2,6 +2,7 @@ package file
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/tomocy/go-todo"
 	"github.com/tomocy/go-todo/infra/rand"
@@ -13,6 +14,19 @@ type sessionRepo struct {
 
 func (r *sessionRepo) NextID(context.Context) (todo.SessionID, error) {
 	return todo.SessionID(rand.GenerateString(30)), nil
+}
+
+func (r *sessionRepo) Pull(context.Context) (*todo.Session, error) {
+	s, err := load(r.fname)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load session: %w", err)
+	}
+
+	if s.Session == nil {
+		return nil, fmt.Errorf("no session")
+	}
+
+	return s.Session.adapt(), nil
 }
 
 func convertSession(src *todo.Session) *session {
