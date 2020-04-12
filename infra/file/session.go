3b768225d8b3e5d8d71297e9d58b43a8d6536d03
tomocy <tomocy.dev@gmail.com>
2020-04-12 15:06:28 +0900

    add convertSession

--- infra/file/session.go
+++ infra/file/session.go
@@ -15,6 +15,13 @@ func (r *sessionRepo) NextID(context.Context) (todo.SessionID, error) {
 	return todo.SessionID(rand.GenerateString(30)), nil
 }
 
+func convertSession(src *todo.Session) *session {
+	return &session{
+		ID:     src.ID(),
+		UserID: src.UserID(),
+	}
+}
+
 type session struct {
 	ID     todo.SessionID `json:"id"`
 	UserID todo.UserID    `json:"user_id"`
