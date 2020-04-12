aa31f5d9ef6be6b4fa1872b01868d7d872b8d622
tomocy <tomocy.dev@gmail.com>
2020-04-12 15:01:39 +0900

    update sessionRepo
    SessionRepo can generate session id.

--- infra/file/session.go
+++ infra/file/session.go
@@ -1,5 +1,16 @@
 package file
 
+import (
+	"context"
+
+	"github.com/tomocy/go-todo"
+	"github.com/tomocy/go-todo/infra/rand"
+)
+
 type sessionRepo struct {
 	fname string
+}
+
+func (r *sessionRepo) NextID(context.Context) (todo.SessionID, error) {
+	return todo.SessionID(rand.GenerateString(30)), nil
 }
