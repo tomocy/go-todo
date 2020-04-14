705d0c4aff07fa6c1aa059ba4274e635aebef68e
tomocy <tomocy.dev@gmail.com>
2020-04-14 22:39:18 +0900

    update app
    App can parse flags.

--- presentation/html/html.go
+++ presentation/html/html.go
@@ -1,8 +1,29 @@
 package html
 
-import "net/http"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
 
 type app struct {
 	*http.ServeMux
 	addr string
+}
+
+func (a *app) parseFlags(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("too less arguments")
+	}
+
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	addr := flags.String("addr", ":80", "the address to listen and serve")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	a.addr = *addr
+
+	return nil
 }
