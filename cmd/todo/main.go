a81361190116fb0d19fa79126d0da50edfe29da1
tomocy <tomocy.dev@gmail.com>
2020-04-14 22:50:30 +0900

    add newRunner

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -5,13 +5,33 @@ import (
 	"os"
 
 	"github.com/tomocy/go-todo/presentation/cli"
+	"github.com/tomocy/go-todo/presentation/html"
 )
 
 func main() {
-	app := cli.New(os.Stdout)
+	app := newRunner()
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+const (
+	modeCLI  = "cli"
+	modeHTML = "html"
+)
+
+func newRunner() runner {
+	mode, ok := os.LookupEnv("TODO_MODE")
+	if !ok {
+		mode = modeCLI
+	}
+
+	switch mode {
+	case modeHTML:
+		return html.New(os.Stdout)
+	default:
+		return cli.New(os.Stdout)
 	}
 }
 
