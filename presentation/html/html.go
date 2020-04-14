7b34c164fe7d58940bbca9c5cf8aada0e01c8d01
tomocy <tomocy.dev@gmail.com>
2020-04-14 22:42:11 +0900

    update app
    App can run.

--- presentation/html/html.go
+++ presentation/html/html.go
@@ -11,7 +11,21 @@ type app struct {
 	addr string
 }
 
-func (a *app) parseFlags(args []string) error {
+func (a *app) Run(args []string) error {
+	if err := a.parse(args); err != nil {
+		return fmt.Errorf("failed to parse: %w", err)
+	}
+
+	a.ServeMux = http.NewServeMux()
+
+	if err := http.ListenAndServe(a.addr, a); err != nil {
+		return fmt.Errorf("failed to listen and serve: %w", err)
+	}
+
+	return nil
+}
+
+func (a *app) parse(args []string) error {
 	if len(args) < 1 {
 		return fmt.Errorf("too less arguments")
 	}
